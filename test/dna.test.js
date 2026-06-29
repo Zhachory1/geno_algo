00a46d3ee55e2d055b753860cf4b563114b67619
Zhach Volker <zhach.volker@rokt.com>
2026-06-29 11:58:34 -0400

    test: add core smoke tests

---
+++ test/dna.test.js
@@ -0,0 +1,32 @@
+const assert = require('node:assert/strict');
+const fs = require('node:fs');
+const path = require('node:path');
+const test = require('node:test');
+const vm = require('node:vm');
+
+function loadDNA(random = () => 0.5) {
+  const context = {random};
+  vm.createContext(context);
+  vm.runInContext(
+      fs.readFileSync(path.join(__dirname, '..', 'dna.js'), 'utf8'),
+      context,
+  );
+  return context;
+}
+
+test('DNA.equals compares move', () => {
+  const {DNA} = loadDNA();
+  const dna = new DNA(10, 20, 30);
+
+  assert.equal(dna.equals(new DNA(10, 20, 30)), true);
+  assert.equal(dna.equals(new DNA(10, 21, 30)), false);
+});
+
+test('combine averages genes when mutation does not happen', () => {
+  const {DNA, combine} = loadDNA();
+  const child = combine(new DNA(10, 20, 30), new DNA(20, 40, 50));
+
+  assert.equal(child.size, 15);
+  assert.equal(child.move, 30);
+  assert.equal(child.dist, 40);
+});
