9ebada25781eb8f038474704e8b0d26a0bb140bf
Zhachory Volker <zhackymoto@gmail.com>
2026-07-15 18:01:16 -0400

    feat: evolve reproduction age
    * fix: add kdtree range query
    
    * perf: query food with kdtree
    
    * perf: query agents with kdtree
    
    * feat: add simulation hud
    
    * feat: add reset population control
    
    * feat: add food attraction dna
    
    * feat: evolve reproduction age
    
    ---------
    
    Co-authored-by: Zhach Volker <zhach.volker@rokt.com>

--- dna.js
+++ dna.js
@@ -7,24 +7,29 @@ const minDist = 50;
 const maxDist = 500;
 const minFoodAttraction = 0.5;
 const maxFoodAttraction = 1.5;
+const minReproduceAge = 200;
+const maxReproduceAge = 800;
 
 function DNA(
     size = random(minSize, maxSize),
     move = random(minMove, maxMove),
     dist = random(minDist, maxDist),
     foodAttraction = random(minFoodAttraction, maxFoodAttraction),
+    reproduceAge = random(minReproduceAge, maxReproduceAge),
 ) {
   this.size = size;
   this.move = move;
   this.dist = dist;
   this.foodAttraction = foodAttraction;
+  this.reproduceAge = reproduceAge;
 
   this.equals = function(other) {
     return (
       this.size == other.size &&
       this.move == other.move &&
       this.dist == other.dist &&
-      this.foodAttraction == other.foodAttraction
+      this.foodAttraction == other.foodAttraction &&
+      this.reproduceAge == other.reproduceAge
     );
   };
 }
@@ -34,6 +39,7 @@ function combine(dna1, dna2) {
   let newMove = 0;
   let newDist = 0;
   let newFoodAttraction = 0;
+  let newReproduceAge = 0;
 
   // size combine or mutate
   if (random() < mutation_rate) {
@@ -67,5 +73,19 @@ function combine(dna1, dna2) {
     newFoodAttraction = (dna1.foodAttraction + dna2.foodAttraction) / 2;
   }
 
-  return new DNA(newSize, newMove, newDist, newFoodAttraction);
+  // reproduce age combine or mutate
+  if (random() < mutation_rate) {
+    newReproduceAge = random(minReproduceAge, maxReproduceAge);
+  } else {
+    // average 1 and 2
+    newReproduceAge = (dna1.reproduceAge + dna2.reproduceAge) / 2;
+  }
+
+  return new DNA(
+      newSize,
+      newMove,
+      newDist,
+      newFoodAttraction,
+      newReproduceAge,
+  );
 }
